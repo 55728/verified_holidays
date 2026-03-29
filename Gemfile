70349336382b6894fd5b4b71557781522851d37f
Kenta Ishizaki <kentaishizaki@55728.jp>
2026-03-29 11:11:38 +0900

    Set up SimpleCov with 90% minimum coverage threshold
    Add SimpleCov to Gemfile and configure in test_helper.rb with branch coverage enabled and 90% minimum threshold for both line and branch coverage. Add coverage/ to .gitignore.

--- Gemfile
+++ Gemfile
@@ -9,5 +9,6 @@ group :development, :test do
   gem 'rake', '~> 13.0'
   gem 'rubocop', '~> 1.0'
   gem 'rubocop-performance', require: false
+  gem 'simplecov', require: false
   gem 'webmock', '~> 3.0'
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter '/test/'
+  minimum_coverage line: 90, branch: 90
+end
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'verified_holidays'
 require 'minitest/autorun'
